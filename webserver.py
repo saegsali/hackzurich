a27a194d0cd3a60a706913e5380e6ccf1446c699
nicofilliol <nicolas.filliol@outlook.com>
2020-09-19 15:31:42 +0200

    update map after upload

--- webserver.py
+++ webserver.py
@@ -2,20 +2,25 @@ from flask import Flask, render_template, request, redirect, url_for
 from string import Template
 import pandas as pd
 from twitter import process_data
+from map import update_map
+
+WORLD_ID = 'XOmfL'
+
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/world')
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html', MAP_ID=WORLD_ID)
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_csv():
     if request.method == 'POST':
         uploaded_files = request.files.getlist("file")
         #file = request.files['file']
-        process_data(uploaded_files)        
+        process_data(uploaded_files) 
+        update_map(WORLD_ID)
         return redirect(url_for('homepage'))
 
 
