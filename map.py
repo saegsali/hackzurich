ad0885c9cb414bcbe0a0cc3ddc93005bb5926780
saegsali <joel@asper.ch>
2020-09-19 10:34:40 +0200

    update map

--- map.py
+++ map.py
@@ -1,17 +1,18 @@
 from datawrapper import Datawrapper
 import pandas as pd
-
+ID = '2q8Wx'
 dw = Datawrapper(access_token = "lHl9w5VaMTL7dkSwCX3IYlUX4weMkOcLBSlzhnChgjnventUlRdOv2sATiHDxTsK")
 
-df = pd.read_csv("test2.csv", sep=',')
-new_chart_info = dw.create_chart(title = 'New world chart 3!', chart_type = 'd3-maps-choropleth', data = df)
+df = pd.read_csv("twitter_parsed.csv", sep=',')
+new_chart_info = dw.create_chart(title = 'Corona Scare Map', chart_type = 'd3-maps-choropleth', data = df)
+#new_chart_info = {'publicId': '2q8Wx', 'language': 'en-US', 'theme': 'default', 'title': 'New world chart 4!', 'type': 'd3-maps-choropleth', 'metadata': {'data': {}}, 'authorId': 252156, 'id': '2q8Wx', 'lastModifiedAt': '2020-09-19T08:09:06.658Z', 'createdAt': '2020-09-19T08:09:06.658Z', 'url': '/v3/charts/2q8Wx'}
 print(new_chart_info)
 
 dw.update_description(
     chart_id = new_chart_info['id'],
-    source_name = 'UN Population Division',
-    source_url = 'https://population.un.org/wup/',
-    byline = 'Your name here!',
+    source_name = 'Crowdbreaks',
+    source_url = 'https://www.crowdbreaks.org/',
+    byline = '',
 )
 
 properties = {
@@ -21,9 +22,20 @@ properties = {
       },
       "visualize": {
           "basemap": "world-2019",
-          "map-key-attr": "DW_STATE_CODE"
+          "map-key-attr": "DW_STATE_CODE",
+          "tooltip": { 
+                "body": "scare level: {{ value }}", 
+                  "title": "{{ country }}", 
+                  "fields": { 
+                        "code": "code", 
+                        "country": "country", 
+                        "value": "value" 
+                    } 
+            } 
+
       }
-    }
+}
+
 dw.update_metadata(new_chart_info['id'], properties)
 
 dw.publish_chart(chart_id = new_chart_info['id'])
