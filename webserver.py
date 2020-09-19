5d246f504329c0eb97467903d1a6f045fdb079fe
saegsali <joel@asper.ch>
2020-09-19 12:40:31 +0200

    add style.css

--- webserver.py
+++ webserver.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def homepage():
     return """
     <h1>Corona Scare Level Map</h1>
+    
+    <link rel="stylesheet" href="style.css">
+    <div class="flight-types">
+        <input type="radio" name="flight-type" value="coach" id="coach" />
+        <label for="coach">
+        Coach
+        </label>
+        <input type="radio" name="flight-type" value="business" id="business" checked />
+        <label for="business">
+        Business
+        </label>
+        <input type="radio" name="flight-type" value="first" id="first" />
+        <label for="first">
+        First
+        </label>
+    </div>
 
     <iframe title="Corona Scare Map" aria-label="Map" id="datawrapper-chart-7nMGn" src="https://datawrapper.dwcdn.net/7nMGn/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="undefined"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();
     </script>
