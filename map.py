6a6fcfe4c27c5f4247e57035542b810376a451b5
saegsali <joel@asper.ch>
2020-09-19 12:22:24 +0200

    updated webserver

--- map.py
+++ map.py
@@ -1,6 +1,6 @@
 from datawrapper import Datawrapper
 import pandas as pd
-ID = '2q8Wx'
+ID = '7nMGn'
 dw = Datawrapper(access_token = "lHl9w5VaMTL7dkSwCX3IYlUX4weMkOcLBSlzhnChgjnventUlRdOv2sATiHDxTsK")
 
 df = pd.read_csv("twitter_parsed.csv", sep=',')
@@ -9,7 +9,8 @@ new_chart_info = dw.create_chart(title = 'Corona Scare Map', chart_type = 'd3-ma
 print(new_chart_info)
 
 dw.update_description(
-    chart_id = new_chart_info['id'],
+    #chart_id = new_chart_info['id'],
+    chart_id = ID,
     source_name = 'Crowdbreaks',
     source_url = 'https://www.crowdbreaks.org/',
     byline = '',
@@ -18,24 +19,85 @@ dw.update_description(
 properties = {
       "axes": {
           "keys": "code",
-          "values": "Normalized_Count"
+          "values": "value"
       },
       "visualize": {
           "basemap": "world-2019",
           "map-key-attr": "DW_STATE_CODE",
           "tooltip": { 
-                "body": "scare level: {{ Normalized_Count }}", 
+                "body": "scare level: {{ value }}", 
                   "title": "{{ Country }}", 
                   "fields": { 
                         "code": "code", 
                         "Country": "Country", 
-                        "Normalized_Count": "Normalized_Count" 
+                        "value": "value" 
                     } 
-            } 
+            },
+        "gradient": {
+        "stops": [
+          {
+            "p": 0,
+            "v": 0
+          },
+          {
+            "p": 0.25,
+            "v": 0.21175258672962177
+          },
+          {
+            "p": 0.5,
+            "v": 1.0358334534177065
+          },
+          {
+            "p": 0.75,
+            "v": 4.514756946019832
+          },
+          {
+            "p": 1,
+            "v": 100
+          }
+        ],
+        "colors": [
+          {
+            "c": "#feebe2",
+            "p": 0
+          },
+          {
+            "c": "#fcc5c0",
+            "p": 0.2
+          },
+          {
+            "c": "#fa9fb5",
+            "p": 0.4
+          },
+          {
+            "c": "#f768a1",
+            "p": 0.6
+          },
+          {
+            "c": "#c51b8a",
+            "p": 0.8
+          },
+          {
+            "c": "#7a0177",
+            "p": 1
+          }
+        ],
+        "domain": [
+          0,
+          0.2,
+          0.4,
+          0.6,
+          0.8,
+          1
+        ]
+      },
+      "zoomable": True
 
       }
 }
 
-dw.update_metadata(new_chart_info['id'], properties)
+#dw.update_metadata(new_chart_info['id'], properties)
+#dw.publish_chart(chart_id = new_chart_info['id'])
 
-dw.publish_chart(chart_id = new_chart_info['id'])
+dw.update_metadata(ID, properties)
+dw.publish_chart(ID)
--- webserver.py
+++ webserver.py
@@ -7,7 +7,7 @@ def homepage():
     return """
     <h1>Corona Scare Level Map</h1>
 
-    <iframe title="New world chart 2!" aria-label="Map" id="datawrapper-chart-3fVVy" src="https://datawrapper.dwcdn.net/3fVVy/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 50% !important; border: none;" height="undefined"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();
+    <iframe title="Corona Scare Map" aria-label="Map" id="datawrapper-chart-7nMGn" src="https://datawrapper.dwcdn.net/7nMGn/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="undefined"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();
     </script>
     """
 
