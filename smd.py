876dfb3ad920bade3569f39e7ee031619cd80f35
nicofilliol <nicolas.filliol@outlook.com>
2020-09-19 22:25:04 +0200

    analyze complete folder

--- smd.py
+++ smd.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import json
+import os
 
-with open('cantons.json') as json_file:
+with open('keywords.json') as json_file:
     CANTONS_DICT = json.load(json_file)
     WORD_LIST = CANTONS_DICT.keys()
     WORD_SET = set(WORD_LIST)
@@ -19,30 +20,46 @@ def find_keywords(row):
     return row
 
 def count(row):
-    row["Total"] = df[row.name].sum()
-    row["Corona Related"] = (df[row.name] & df["Corona"]).sum()
+    row["Total"] += df[row.name].sum()
+    row["Corona Related"] += (df[row.name] & df["Corona"]).sum()
     return row
 
-df = pd.read_csv("/Users/Nico/Desktop/hackzurich_data/SMD/zwao_output.csv")
-df["fulltext"] = df["ht"].astype("str") + " " + df["sm"].astype("str") + " " + df["tx"].astype("str")
+
+
+# Initialize necessary dataframes
 ch_data = pd.read_csv("switzerland_data.csv")
 codes = ch_data["Postal"].values
 
-# Add columns and initialize to 0
-for col in codes:
-    df[f"{col}"] = 0
-df["Corona"] = 0
-
-# Apply keywords
-df = df.apply(find_keywords, axis=1)
-
-# Count total mentions of canton and news related to Corona
 news_data = pd.DataFrame(index=codes)
 news_data["Corona Related"] = 0
 news_data["Total"] = 0
 
-news_data = news_data.apply(count, axis=1)
+# Loop through files in SMD folder
+directory = "/Users/Nico/Desktop/hackzurich_data/SMD/data_export"
+for filename in os.listdir(directory):
+    print(filename)
+    if filename.endswith(".csv"): 
+        path = os.path.join(directory, filename)
+
+        # Read news data and process
+        df = pd.read_csv(path)
+        df["fulltext"] = df["ht"].astype("str") + " " + df["sm"].astype("str") + " " + df["tx"].astype("str")
+        
+        # Reset columns to 0
+        for col in codes:
+            df[f"{col}"] = 0
+        df["Corona"] = 0
+        
+        # Apply keywords
+        df = df.apply(find_keywords, axis=1)
+
+        # Count total mentions of canton and news related to Corona
+        news_data = news_data.apply(count, axis=1)
+
+news_data["Ratio"] = news_data["Corona Related"] / news_data["Total"] * 100
 print(news_data)
+
+
 
 
 
